a0440f7d95cdfcc1b5ecb10ab4eb75d413d233a0
Florian Körner <contact@florian-koerner.com>
2018-10-13 12:39:36 +0200

    Fix: Mouth color

--- src/sprites.ts
+++ src/sprites.ts
@@ -101,20 +101,20 @@ export default function(options: Options = {}) {
     if (false === Array.isArray(options.mood) || options.mood.indexOf('sad') > -1) {
       // prettier-ignore
       mouth.push(
-        `<path d="M8 13h3v1H8v-1z" fill="${mouthColor}"/>`,
-        `<path d="M8 13h4v1H8v-1z" fill="${mouthColor}"/>`,
-        `<path d="M9 13h2v1H9v-1z" fill="${mouthColor}"/>`,
-        `<path d="M8 12v1h3v1h1v-1h-1v-1H8z" fill="${mouthColor}"/>`,
-        `<path d="M8 13v1h1v-1h3v-1H9v1H8z" fill="${mouthColor}"/>`
+        `<path d="M8 13h3v1H8v-1z" fill="${mouthColor.hex}"/>`,
+        `<path d="M8 13h4v1H8v-1z" fill="${mouthColor.hex}"/>`,
+        `<path d="M9 13h2v1H9v-1z" fill="${mouthColor.hex}"/>`,
+        `<path d="M8 12v1h3v1h1v-1h-1v-1H8z" fill="${mouthColor.hex}"/>`,
+        `<path d="M8 13v1h1v-1h3v-1H9v1H8z" fill="${mouthColor.hex}"/>`
       );
     }
 
     if (false === Array.isArray(options.mood) || options.mood.indexOf('happy') > -1) {
       // prettier-ignore
       mouth.push(
-        `<path d="M7 12v1h1v1h4v-1H8v-1H7z" fill="${mouthColor}"/>`,
-        `<path d="M10 12v1H9v1h2v-2h-1z" fill="${mouthColor}"/>`,
-        `<path d="M8 13v1h4v-1h1v-1h-1v1H8z" fill="${mouthColor}"/>`,
+        `<path d="M7 12v1h1v1h4v-1H8v-1H7z" fill="${mouthColor.hex}"/>`,
+        `<path d="M10 12v1H9v1h2v-2h-1z" fill="${mouthColor.hex}"/>`,
+        `<path d="M8 13v1h4v-1h1v-1h-1v1H8z" fill="${mouthColor.hex}"/>`,
         `<path d="M8 12v2h4v-2H8z" fill="#FFF"/>`
       );
     }
@@ -122,8 +122,8 @@ export default function(options: Options = {}) {
     if (false === Array.isArray(options.mood) || options.mood.indexOf('surprised') > -1) {
       // prettier-ignore
       mouth.push(
-        `<path d="M9 12v2h2v-2H9z" fill="${mouthColor}"/>`,
-        `<path d="M9 13v1h1v-1H9z" fill="${mouthColor}"/>`
+        `<path d="M9 12v2h2v-2H9z" fill="${mouthColor.hex}"/>`,
+        `<path d="M9 13v1h1v-1H9z" fill="${mouthColor.hex}"/>`
       );
     }
 
